328cd463e57fd6a203c01429aee7e67d62dec590
aleksei <1alekseik1@gmail.com>
2018-10-02 23:58:17 +0300

    __init__ for `DoubleLinkedList`. Class for List node

---
+++ app/double_linked_list.py
@@ -0,0 +1,11 @@
+class DoubleLinkedList:
+    class Item:
+
+        def __init__(self, value=None, prev=None, next=None):
+            self.value = value
+            self.prev = prev
+            self.next = next
+
+    def __init__(self):
+        self.length = 0
+        self.start_item = DoubleLinkedList.Item()
