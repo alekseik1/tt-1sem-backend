3bd7c19c88e9f2019dd05a889617104a8c0b688e
alekseik1 <1alekseik1@gmail.com>
2018-10-15 19:18:32 +0300

    УДАЛИЛ все файлы, кроме flask-кусков

--- app/main.py
+++
@@ -1,9 +0,0 @@
-def wsgi_application(environ, start_response):
-    status = "200 OK"
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = 'Hello, world meme!'.encode('utf-8')
-    print(environ['wsgi.version'])
-    start_response(status, headers)
-    return [body]
