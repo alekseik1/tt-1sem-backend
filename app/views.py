04868f76085fcd857f732289d3e704cfe6f51c22
alekseik1 <1alekseik1@gmail.com>
2018-10-29 22:37:34 +0300

    Удалил handlers.py. Весь его функционал перенес в views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import request, jsonify, make_response, render_template
+from flask import request, jsonify
 from app import app
+from app import model
 import json
 
 
@@ -57,3 +58,35 @@ def get_user_contacts(user="Nobody"):
     """
     return create_stub_answer(request, 200)
 
+
+@app.route('/messages/')
+def messages():
+    chat_id = int(request.args.get('chat_id'))
+    messages = model.list_messages_by_chat(chat_id, 10)
+    return jsonify(messages)
+
+
+@app.route('/find_user/')
+def find_user():
+    user_name = request.args.get('user_name', None, type=str)
+    users = []
+    if user_name:
+        users = model.find_user(user_name=user_name)
+    nick = request.args.get('nick', None, type=str)
+    if nick:
+        users = model.find_user(nick=nick)
+    user_id = request.args.get('user_id', None, type=int)
+    if user_id:
+        users = model.find_user(user_id=user_id)
+    return jsonify(users)
+
+
+@app.route('/create_chat/', methods=['POST', 'GET'])
+def create_chat():
+    topic = str(request.args.get('topic'))
+    is_group_chat = int(request.args.get('is_group'))
+    model.create_chat(topic, is_group_chat)
+    return jsonify({
+        'topic': topic,
+        'is_group_chat': is_group_chat
+    })
