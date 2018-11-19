c4db1cbbef5b18d5dd8a822b4c793258d1436c63
alekseik1 <1alekseik1@gmail.com>
2018-11-19 19:23:38 +0300

    id в тестах в отдельной переменной

--- tests/jsonrpc_test.py
+++ tests/jsonrpc_test.py
@@ -2,6 +2,10 @@ import unittest
 from app import app, jsonify
 from flask_jsonrpc.proxy import ServiceProxy
 
+# Меняйте их от теста к тесту, я лучше пока не придумал
+user_id = 2
+chat_id = 143
+
 
 class JsonrpcTest(unittest.TestCase):
 
@@ -10,14 +14,14 @@ class JsonrpcTest(unittest.TestCase):
 
     def test_send_message(self):
         request_json = {'method': 'send_message', 'params': {
-                'chat_id': 113, 'user_id': 2, 'content': 'TestMessage'
+                'chat_id': chat_id, 'user_id': user_id, 'content': 'TestMessage'
             }, 'id': 1}
         a = self.app.post('/api/', json=request_json)
         self.assertNotEqual(a, None)
 
     def test_get_messages_by_chat(self):
         request_json = {'method': 'get_messages_by_chat', 'params': {
-            'chat_id': 113
+            'chat_id': chat_id
         }, 'id': 2}
         a = self.app.post('/api/', json=request_json)
         self.assertNotEqual(a, None)
