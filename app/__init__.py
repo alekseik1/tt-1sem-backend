01ff940861c69509f5ac761d6ce25bdf651c8d42
alekseik1 <alekseik1@yandex.ru>
2019-02-17 17:38:54 +0300

    fix: ошибки CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from instance.config import DevelopmentConfig as dev_config
 from flask_jsonrpc import JSONRPC
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(dev_config)
 
 jsonrpc = JSONRPC(app, '/api/')
