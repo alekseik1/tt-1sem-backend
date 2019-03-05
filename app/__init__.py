df8a6450bb189333ca653dc58216d9e1011dede9
alekseik1 <1alekseik1@gmail.com>
2019-03-05 13:12:28 +0300

    Use Manager to control app. Connect Alchemy to DB

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,21 @@ from instance.config import DevelopmentConfig as dev_config
 from flask_jsonrpc import JSONRPC
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(dev_config)
 # Настроим БД
-app.config['SQL_ALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{url}:{port}/{db_name}'\
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{url}:{port}/{db_name}'\
     .format(username=dev_config.DB_USER, password=dev_config.DB_PASS, url=dev_config.DB_HOST,
             port=dev_config.DB_PORT, db_name=dev_config.DB_NAME)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 jsonrpc = JSONRPC(app, '/api/')
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from .views import *
