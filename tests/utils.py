fba27836cb5fa159ff3f20b23b898befee0cdb3b
alekseik1 <1alekseik1@gmail.com>
2019-03-05 09:47:25 +0300

    Методы для создания/уничтожения БД

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,8 @@
 # TODO: перенести сюда эталонные тесты и константы. Из тестового класса.
 
-from instance.config import DevelopmentConfig as config
+from instance.config import TestingConfig as config
+from instance.config import DevelopmentConfig as dev_config
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 USER_IDS = range(2, 10)
 CHAT_IDS = range(2, 10)
@@ -72,10 +74,107 @@ def fill_messages():
 
 
 def init_all():
+    create_db()
+    fill_tables()
     fill_users()
     fill_chats()
     fill_members()
     fill_messages()
+
+
+def deinit_all():
+    delete_db()
+
+
+def fill_tables():
+    import psycopg2.extras
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER,
+                            password=config.DB_PASS, host=config.DB_HOST)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("""
+    CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    nick TEXT NOT NULL UNIQUE
+        CHECK (length(nick) < 32),
+    name TEXT NOT NULL
+        CHECK (length(name) < 32),
+    avatar TEXT NOT NULL
+);
+
+CREATE TABLE chats (
+  chat_id SERIAL PRIMARY KEY,
+  is_group_chat INTEGER,
+  topic TEXT NOT NULL
+    CHECK (length(topic) < 1000),
+  added_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE messages (
+    message_id SERIAL PRIMARY KEY,
+    chat_id INTEGER NOT NULL
+      REFERENCES chats(chat_id),
+    user_id INTEGER NOT NULL
+        REFERENCES users(user_id),
+    content TEXT NOT NULL
+        CHECK (length(content) < 65536),
+    added_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE members (
+  user_id INTEGER NOT NULL
+    REFERENCES users(user_id),
+  chat_id INTEGER NOT NULL
+    REFERENCES chats(chat_id),
+  new_messages INTEGER NOT NULL,
+  last_read_message_id INTEGER
+    REFERENCES messages(message_id)
+);
+
+CREATE TABLE attachments (
+  attach_id SERIAL PRIMARY KEY,
+  chat_id INTEGER NOT NULL
+    REFERENCES chats(chat_id),
+  user_id INTEGER NOT NULL
+    REFERENCES users(user_id),
+  message_id INTEGER NOT NULL
+    REFERENCES messages(message_id),
+  type TEXT NOT NULL
+    CHECK (length(type) < 1000),
+  url TEXT NOT NULL
+);
+    """)
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+
+def create_db():
+    import psycopg2.extras
+    conn = psycopg2.connect(dbname=dev_config.DB_NAME, user=dev_config.DB_USER,
+                            password=dev_config.DB_PASS, host=dev_config.DB_HOST)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor.execute("""
+    CREATE DATABASE {}
+    """.format(config.DB_NAME))
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+
+def delete_db():
+    import psycopg2.extras
+    conn = psycopg2.connect(dbname=dev_config.DB_NAME, user=dev_config.DB_USER,
+                            password=dev_config.DB_PASS, host=dev_config.DB_HOST)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor.execute("""
+    DROP DATABASE {}
+    """.format(config.DB_NAME))
+    cursor.close()
+    conn.commit()
+    conn.close()
 
 
 def ordered(obj):
@@ -93,3 +192,4 @@ def equals_json(json1, json2):
 
 if __name__ == '__main__':
     init_all()
+    #delete_db()
