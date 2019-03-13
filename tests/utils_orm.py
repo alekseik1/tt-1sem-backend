bd43e8070c2de18b0ac992a9311ebec678c5e985
alekseik1 <1alekseik1@gmail.com>
2019-03-13 16:44:20 +0300

    fix: run multiple tests

--- tests/utils_orm.py
+++ tests/utils_orm.py
@@ -6,24 +6,24 @@ USER_IDS = range(2, 10)
 CHAT_IDS = range(2, 10)
 MATRIX = [[2, 3], [2, 4], [3, 5], [4, 6], [5, 7], [6, 8], [7, 9], [9, 8]]
 
-users = [User(nick='test_user_{}'.format(user_id),
-              name='tester_{}'.format(user_id),
-              avatar='REKLAMA') for user_id in USER_IDS]
-chats = [Chat(is_group=0, topic='Gen chat_{}'.format(chat_id)) for chat_id in CHAT_IDS]
-messages = []
-
 
 def fill_users():
+    users = [User(nick='test_user_{}'.format(user_id),
+                  name='tester_{}'.format(user_id),
+                  avatar='REKLAMA') for user_id in USER_IDS]
     db.session.add_all(users)
     db.session.commit()
+    return users
 
 
 def fill_chats():
+    chats = [Chat(is_group=0, topic='Gen chat_{}'.format(chat_id)) for chat_id in CHAT_IDS]
     db.session.add_all(chats)
     db.session.commit()
+    return chats
 
 
-def fill_members():
+def fill_members(users, chats):
     i = 0
     for user in users:
         user.chats = chats[i:i+3]
@@ -31,16 +31,16 @@ def fill_members():
     db.session.commit()
 
 
-def fill_messages():
-    global messages
+def fill_messages(chats):
     messages = [Message(chat=chat, user=chat.users[0], content='Test message') for chat in chats]
     db.session.add_all(messages)
     db.session.commit()
+    return messages
 
 
 def fill_all():
-    fill_users()
-    fill_chats()
-    fill_members()
-    fill_messages()
-    db.session.commit()
+    users = fill_users()
+    chats = fill_chats()
+    fill_members(users, chats)
+    messages = fill_messages(chats)
+    return users, chats, messages
--- tests/views_test.py
+++ tests/views_test.py
@@ -3,17 +3,15 @@ from app import db
 from tests.utils_orm import fill_all
 from app.views import *
 from app.model import User, Chat
-from tests.utils_orm import USER_IDS, chats, users
+from tests.utils_orm import USER_IDS
 import time
 
 
 class ViewsMethodsTest(TestCase):
 
     def setUp(self):
-        # Почему-то в tearDown зависает drop_all(), будет теперь здесь
-        db.drop_all()
         db.create_all()
-        fill_all()
+        self.users, self.chats, self.messages = fill_all()
 
     def test_create_chat(self):
         CHAT_TOPIC = 'chat_topic_1'
@@ -31,19 +29,19 @@ class ViewsMethodsTest(TestCase):
 
     def test_get_chat_messages(self):
         # Проверим, что после инициализации список сообщений не пуст
-        self.assertNotEqual(len(get_chat_messages(chats[0].id)), 0)
+        self.assertNotEqual(len(get_chat_messages(self.chats[0].id)), 0)
         # Добавим руками сообщение и проверим, появится ли оно
         CONTENT = 'Check!'
-        new_mess = Message(chat=chats[0], user=chats[0].users[0], content=CONTENT)
+        new_mess = Message(chat=self.chats[0], user=self.chats[0].users[0], content=CONTENT)
         db.session.add(new_mess)
         db.session.commit()
         # Оно будет последним, проверим его
-        message_json = get_chat_messages(chats[0].id)[-1]
-        self.assertEqual(message_json['user_id'], str(chats[0].users[0].id))
+        message_json = get_chat_messages(self.chats[0].id)[-1]
+        self.assertEqual(message_json['user_id'], str(self.chats[0].users[0].id))
         self.assertEqual(message_json['content'], CONTENT)
 
     def test_leave_chat(self):
-        user, chat = users[-1], users[-1].chats[-1]
+        user, chat = self.users[-1], self.users[-1].chats[-1]
         user_id, chat_id = user.id, chat.id
         with self.subTest("Chat is really deleted"):
             # Выйдем
@@ -54,6 +52,10 @@ class ViewsMethodsTest(TestCase):
             chat_id = 10**5
             with self.assertRaises(ValueError):
                 leave_chat(chat_id=chat_id, user_id=user_id)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
 if __name__ == '__main__':
