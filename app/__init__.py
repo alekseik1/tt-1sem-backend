d2e9429c9f80abd0fdd056c75cccb9480e6854e2
alekseik1 <1alekseik1@gmail.com>
2019-03-19 20:07:59 +0300

    Celery basic setup

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from app.flask_celery import make_celery
 
 app = Flask(__name__)
 CORS(app)
@@ -14,10 +15,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{url
     .format(username=config.DB_USER, password=config.DB_PASS, url=config.DB_HOST,
             port=config.DB_PORT, db_name=config.DB_NAME)
 
+# Настроим Celery
+app.config.update(
+    broker_url=config.broker_url, result_backend=config.result_backend,
+)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jsonrpc = JSONRPC(app, '/api/')
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+celery = make_celery(app)
 
 from .views import *
